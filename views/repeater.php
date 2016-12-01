1d5396a5dddefd0d7c17792d2cf1e4ff7320d2ea
Ariel Braun <relbraun@gmail.com>
2016-12-01 17:52:28 +0200

    repeater wrapper view

--- views/repeater.php
+++ views/repeater.php
@@ -4,6 +4,9 @@
 ?>
 
 <div class="repeater-item" data-id="<?= $model->primaryKey ?>">
+    <?php if(isset($contentPath)){
+        $content = $this->render($contentPath, ['model' => $model]);
+    } ?>
     <?= $content ?>
     <a class="remove" href="javascript:;" >X</a>
 </div>
